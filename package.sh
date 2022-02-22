b02feb99f275af601bdad17b5fbd9fbdf745787b
txoof <aaron.ciuffo@gmail.com>
2022-02-22 12:21:44 +0100

    add version number to zip

--- package.sh
+++ package.sh
@@ -24,8 +24,16 @@ then
     abort "Directory \`$1\` does not appear to exist"
 fi
 
+
+
+
+
 TIME=$(date "+%Y%m%d_%H%M%S")
-PACKAGENAME=$(basename $1)-$TIME.zip
 pushd $1
-zip -X -r  ../$PACKAGENAME ./* -x "*.md"
-popd
+VERSION=$(grep -Eo '^\s+version="(.*)"' ./plugin.xml | sed -ne 's/[^0-9]*\(\([0-9]\.\)\{0,5\}[0-9]\{0,8\}[^.]\).*".*/\1/p')
+PACKAGENAME=$(basename $1)-V$VERSION-$TIME.zip
+# echo $VERSION
+zip -X -r  ../$PACKAGENAME ./* -x "*.md" 
+echo ""
+echo "CREATED PLUGIN: $PACKAGENAME"
+popd > /dev/null 2>&1
