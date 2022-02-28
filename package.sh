7355c6fe578be1093ad2ab45188e5b4d1c85ae42
txoof <aaron.ciuffo@gmail.com>
2022-02-28 15:18:25 +0100

    ignore all dot files

--- package.sh
+++ package.sh
@@ -33,7 +33,7 @@ pushd $1
 VERSION=$(grep -Eo '^\s+version="(.*)"' ./plugin.xml | sed -ne 's/[^0-9]*\(\([0-9]\.\)\{0,5\}[0-9]\{0,8\}[^.]\).*".*/\1/p')
 PACKAGENAME=$(basename $1)-V$VERSION-$TIME.zip
 # echo $VERSION
-zip -X -r  ../$PACKAGENAME ./* -x "*.md" 
+zip -X -r  ../$PACKAGENAME ./* -x "*.md" -x ".*"
 echo ""
 echo "CREATED PLUGIN: $PACKAGENAME"
 popd > /dev/null 2>&1
